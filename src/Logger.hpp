68b89dd5a8ead54ddf3927bac58d4ebe6bf1266a
mohammad-alow <md-alow@hotmail.com>
2015-12-04 21:46:10 +0200

    lexer logger + minor changes

---
+++ src/Logger.hpp
@@ -0,0 +1,28 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <iostream>
+#include<fstream>
+
+using namespace std;
+
+class Logger {
+public:
+  Logger(string file_name)  {
+    os.open(file_name.c_str(), ofstream::out);
+  }
+
+  ~Logger() {
+	  os.close();
+  }
+
+  void log(string s, bool new_line = true) {
+	  os<<s;
+    if (new_line) os<<endl;
+    else os<<" ";
+  }
+
+private:
+  ofstream os;
+};
+#endif
