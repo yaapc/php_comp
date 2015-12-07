1095e52414e202ed985963fb68220632777b8ca3
mohammad-alow <md-alow@hotmail.com>
2015-12-07 11:45:48 +0200

    reding input from index.php

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include "definitions.hpp"
 #include "grammar.hpp"
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 int main(int argc, char** argv) {
   cout<<"php-compiler"<<endl<<endl;
+
+  extern FILE *yyin;
+  if (!(yyin = fopen("index.php", "r"))) {
+    cerr<<"index.php not found"<<endl;
+    return 1;
+  }
+
   yyparse();
+
+  cout<<"compilation done"<<endl;
   return 0;
 }
