31338428804804dc285a68767e91edf2050e13fd
Bassel Shmali <bass3l@live.com>
2015-12-19 18:22:34 +0200

    Create index.php
    the sample file of what is done

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+$iAmUndecl;
+
+
+int $iAmInt,$iAmInt2,$iAmInt;
+float $iAmFloat = 1.5;
+iAmClass $iAmClassObject;
+
+function iAmAFunction(int $iAmIntParam, float $iAmFloatParam, $iAmObjParam, int $iAmADefParm = 0) : void {
+	string $iAmString = "Hello World!";	
+	
+}
+function iAmFunction2(double $iAmDoubleParam) : string {
+	bool $iAmBool;
+}
+
+function iAmFunction3(double $iAmDefault = 10) : void{
+	
+}
+
+final class iAmClass extends iAmBaseClass {
+	protected static int $iAmMember;
+	
+	public function getIAmMember() : int {
+			char $iAmChar;
+			
+			for(int $i =0; $i < 10; $i++){
+				int $iAmInForLoop = 9999;
+			}
+			
+			do {
+				int $iAmInDoWhileLoop = 999;
+			}while ($iAmChar = 0);
+			
+			if($iAmChar){
+				int $iAmInASingleIf;	
+			}
+			
+			if(true)
+				int $iAmInASingleIf2;
+			
+			if($iAmChar = 'd'){
+				int $iAmInAMultiIf;
+			}else if($iAmChar = 'f'){
+				int $iAmInAMultiIf2;
+			}else 
+				int $iAmInASingleElse;
+			
+	}   
+
+	private function setIAmMember(int $param1, string $param2) : void;
+	
+}
+
+
+abstract class iAmBaseClass {
+	private int $iAmInnerInt;
+	private function iAmBaseClass();
+}
+
+
+while (true)
+	echo "yes";
+
+
+	{
+		
+		int $iAmAlone;
+		
+	}
+
+?>
