de85ce5428e74160f0abfd3c61409b4781e672ab
Bassel Shmali <bass3l@live.com>
2015-12-19 16:53:42 +0200

    first update with SymbolTable definitions

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -1,6 +1,9 @@
+#pragma once
 #ifndef DEFINITIONS_HPP
 #define DEFINITIONS_HPP
-#include "ErrorRevovery.h"
+
+#include "ErrorRecovery\ErrorRecovery.h"
+#include "SymbolTable\SymbolsParser.h"
 
 int yylex(void);
 int yyparse();
@@ -10,7 +13,19 @@ enum primitive {
   T_PRIMITIVE_INTEGER = 1,T_PRIMITIVE_FLOAT,T_PRIMITIVE_DOUBLE,T_PRIMITIVE_BOOLEAN,T_PRIMITIVE_STRING,T_PRIMITIVE_CHAR, T_PRIMITIVE_VOID
 };
 
+static const enum SymbolTypes { 
+	FUNCTION = 1, CLASS, OBJECT, VARIABLE, INTERFACE, TYPE, DATA_MEMBER, METHODS, CASE_LABEL, PARAMETER
+};
+static const enum AccessModifiers { 
+	DEFAULT_ACCESS = 1, PUBLIC_ACCESS, PRIVATE_ACCESS, PROTECTED_ACCESS 
+};
+static const enum StorageModifier { 
+	DEFAULT_STORAGE = 1, FINAL_STORAGE, STATIC_STORAGE, FINAL_STATIC_STORAGE
+};
+
+
 enum open_tag_type {
   NORMAL, WITH_ECHO
 };
+
 #endif
