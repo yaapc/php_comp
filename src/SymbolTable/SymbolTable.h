f917534598ad1578c4d76bd2a5d11118d1a02ebf
Bassel Shmali <bass3l@live.com>
2015-12-19 17:03:55 +0200

    Create SymbolTable.h

---
+++ src/SymbolTable/SymbolTable.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef SYMBOLTABLE_H
+#define SYMBOLTABLE_H
+
+#include"Symbol.h"
+#define MAX_LENGTH 71 // symbols hash map "M"
+
+//file headers
+#include<iostream>
+#include<fstream>
+#include<string>
+
+class Scope;
+
+class SymbolTable {
+public:
+	SymbolTable();
+	~SymbolTable();
+
+	Symbol* insert(Symbol* symbol);
+	Symbol* lookup(char* name);
+
+	string toString();
+
+	void setOwnerScope(Scope* owner);
+	Scope* getOwnerScope();
+
+private:
+
+	Symbol **map;
+	int hash(char * name); // the hash function
+	Symbol* get(int index);
+	Symbol* put(int index, Symbol* symbol);
+
+	Scope* ownerScope; //the scope which owns the SymbolTable
+};
+
+#endif
