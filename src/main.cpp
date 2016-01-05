065f8e0f45a6354ce7c428696a63a5f69ff0520e
Bassel Shmali <bass3l@live.com>
2016-01-05 19:53:57 +0200

    added dependency graph
    added the pointer to TypeChecker
    called checkDependency()
    printing Error Queue moved here from grammer.y

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,38 @@
 #include "definitions.h"
-#include "grammar.hpp"
+#include "grammer.hpp"
 #include "generate_dot.hpp"
+#include "TypeChecker.h"
+
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
 
 using namespace std;
-
 extern void initSymbolsParser();
 extern SymbolsParser * symbolsParser;
+extern TypeChecker * typeChecker;
 extern void initTypeChecker();
 
 int main(int argc, char** argv) {
-  initSymbolsParser();
-  initTypeChecker();
-  cout<<"php-compiler"<<endl<<endl;
+	initSymbolsParser();
+	initTypeChecker();
+	cout << "php-compiler" << endl << endl;
+	extern FILE *yyin;
+	if (!(yyin = fopen("index.php", "r"))) {
+		cerr << "index.php not found" << endl;
+		return 1;
+	}
+	yyparse();
+	
+	symbolsParser->printSymbolTables(); // log symbol table
+	typeChecker->checkDependency(); // check circular dependency and create dependency graph 
+	errorRec.printErrQueue();
 
-  extern FILE *yyin;
-  if (!(yyin = fopen("index.php", "r"))) {
-    cerr<<"index.php not found"<<endl;
-    return 1;
-  }
-
-  yyparse();
-  symbolsParser->printSymbolTables();
+	//Visualizing
 	ofstream dot_file("symbol_table.dot");
-  generate_dot(symbolsParser->getRootScope(), dot_file);
+	generate_dot(symbolsParser->getRootScope(), dot_file);
 	dot_file.close();
-  cout<<"compilation done"<<endl;
-  return 0;
+
+	cout << "compilation done" << endl;
+	return 0;
 }
