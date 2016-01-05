960d220f895358001273bab090e9c19757bd6df4
Bassel Shmali <bass3l@live.com>
2016-01-05 20:03:56 +0200

    added dependency graph

--- src/TypeChecker.h
+++ src/TypeChecker.h
@@ -1,12 +1,17 @@
 #pragma once
-#include"ErrorRecovery\ErrorRecovery.h"
+#ifndef TYPECHECKER_H
+#define TYPECHECKER_H
+
+#include"DependencyGraph\DependencyGraph.h"
 #include"SymbolTable\SymbolsParser.h"
+
 
 class TypeChecker {
 public:
 	TypeChecker(ErrorRecovery* errRecovery, SymbolsParser* symbolsParser){
 		this->errRecovery = errRecovery;
 		this->symbolsParser = symbolsParser;
+		this->dg = new DependencyGraph(errRecovery);
 	}
 	~TypeChecker();
 
@@ -14,7 +19,16 @@ public:
 		Symbol* sym = this->symbolsParser->lookUpSymbol(name, lineNo, colNo);
 	}
 
+	void checkDependency();
+
 private:
 	ErrorRecovery* errRecovery;
 	SymbolsParser* symbolsParser;
+
+	void searchScope(Scope* scope);
+	void parseScope(Scope* scope);
+	DependencyGraph* dg;
 };
+
+
+#endif
