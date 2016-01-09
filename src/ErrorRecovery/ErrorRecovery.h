627466e95ed2709a3afac75fbeb19c8f2a2291ee
Bassel Shmali <bass3l@live.com>
2016-01-10 00:21:30 +0200

    added remove(), added id

--- src/ErrorRecovery/ErrorRecovery.h
+++ src/ErrorRecovery/ErrorRecovery.h
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+static int idCounter = 0;
 class ErrRecItem{
 public:
 	int lineNo;
@@ -14,15 +15,17 @@ public:
 	char* txt;
 	char* var;
 	ErrRecItem * next;
+	int id;
 };
 
 class ErrRecQueue{
 public:
 	ErrRecItem * front;
 	ErrRecItem * tail;
-	void enqueue(int lineNo, int colNo, const char *txt, const char *varName)
+	int enqueue(int lineNo, int colNo, const char *txt, const char *varName)
 	{
 		ErrRecItem * item = new ErrRecItem();
+		item->id = idCounter++;
 		item->colNo = colNo;
 		item->lineNo = lineNo;
 		item->txt = new char[255];
@@ -31,35 +34,36 @@ public:
 		item->var = new char[255];
 		item->var[0] = '\0';
 		strcat(item->var, varName);
-		if(front == 0){
+		if (front == 0){
 			front = tail = item;
 		}
 		else{
 			tail->next = item;
 			tail = item;
 		}
+		return item->id;
 	}
 	ErrRecItem * dequeue()
 	{
 		ErrRecItem * ret = 0;
-		if(front != 0)
+		if (front != 0)
 		{
 			ret = front;
-			if(front == tail)
+			if (front == tail)
 			{
 				front = tail = 0;
 			}
 			else
-	     	{
-			front = front->next;
-		    }
+			{
+				front = front->next;
+			}
 		}
 
 		return ret;
 	}
 	int isEmpty()
 	{
-		if(front == 0 )
+		if (front == 0)
 			return 1;
 		else
 			return 0;
@@ -83,25 +87,53 @@ public:
 		os << "----------------Parser Error----------------" << endl;
 		if (errQ->isEmpty()){
 			os << "No Error..." << endl;
-		}else{
-				while(!errQ->isEmpty()){
-					ErrRecItem * e = errQ->dequeue();
-					os<<"Error: ["<<e->lineNo<<","<<e->colNo<<"]   "<<e->txt<<",  "<<e->var<<"\n";
-				}
+		}
+		else{
+			while (!errQ->isEmpty()){
+				ErrRecItem * e = errQ->dequeue();
+				os << "Error: [" << e->lineNo << "," << e->colNo << "]   " << e->txt << ",  " << e->var << "\n";
+			}
 		}
 
 		os << "----------------Parser Warning--------------" << endl;
 
 		if (warnQ->isEmpty()){
 			os << "No Waring..." << endl;
-		}else{
-			while(!warnQ->isEmpty()){
+		}
+		else{
+			while (!warnQ->isEmpty()){
 				ErrRecItem * e = warnQ->dequeue();
-				os<<"Warning: ["<<e->lineNo<<","<<e->colNo<<"]   "<<e->txt<<",  "<<e->var<<"\n";
+				os << "Warning: [" << e->lineNo << "," << e->colNo << "]   " << e->txt << ",  " << e->var << "\n";
 			}
 		}
-		os << "--------------------------------------------" <<endl;
+		os << "--------------------------------------------" << endl;
 	}
+
+	void removeFromQueue(int id){
+		ErrRecItem* qWalker = this->errQ->front;
+		ErrRecItem* prev = qWalker;
+		bool found = false;
+		while (!found && qWalker != 0){
+			if (qWalker->id == id){
+				if (prev == qWalker){ // first item
+					this->errQ->front = qWalker->next;
+					found = true;
+				}
+				else if (qWalker->next == 0){
+					prev->next = 0;
+					this->errQ->tail = prev;
+					found = true;
+				}
+				else{
+					prev->next = qWalker->next;
+					found = true;
+				}
+			}
+			prev = qWalker;
+			qWalker = qWalker->next;
+		}
+	}
+
 };
 
 #endif
