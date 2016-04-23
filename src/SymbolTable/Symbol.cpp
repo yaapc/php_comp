dbb13dc4a542691f034d4bba16430330b05e73ad
Bassel Shmali <bass3l@live.com>
2016-04-22 19:36:21 -0700

    added generateTypeExpression() to functions

--- src/SymbolTable/Symbol.cpp
+++ src/SymbolTable/Symbol.cpp
@@ -164,6 +164,12 @@ vector<Parameter*> Function::parameters() {
 	}
 	return res;
 }
+
+string Function::generateTypeExpression() {
+	return "hello world";
+}
+
+
 /*
 ========================================
 CLASS:
