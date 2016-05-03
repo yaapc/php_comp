7e2f1dc2d0789a48a114839499fed1c4856be926
Bassel Shmali <bass3l@live.com>
2016-05-03 13:49:19 -0700

    removed TypeCharacter from TypeSystemHelper

--- src/TypeSystem/TypeSystemHelper.cpp
+++ src/TypeSystem/TypeSystemHelper.cpp
@@ -6,7 +6,6 @@ string TypeSystemHelper::getTypeName(int typeId) {
 	case VOID_TYPE_ID: return "Void";
 	case BOOLEAN_TYPE_ID: return "Boolean";
 	case FLOAT_TYPE_ID: return "Float";
-	case CHARACTER_TYPE_ID: return "Char";
 	case STRING_TYPE_ID: return "String";
 	default: return "TS_HELPER_ERROR";
 	}
