3788b0304698accaca873981b622e036c7d826a2
Mohammed Ghanem <master.mg95@gmail.com>
2016-06-03 14:20:33 +0300

    While typeChecking
    To Be check by you (bassel)

--- src/AST/WhileNode.cpp
+++ src/AST/WhileNode.cpp
@@ -3,12 +3,14 @@
 #include "../Code Generator/CodeGeneratorVistor.hpp"
 #include "../Code Generator/OptimizationVistor.hpp"
 #include "AST_Visitors\TypeErrorVisitor.hpp"
+#include "../TypeSystem/TypesTable.h"
 
 WhileNode::WhileNode(Node *condition, Node *body, int line, int col) : condition(condition) {
 	this->body = body;
 	this->body->hasScopeFrame = true;
 	this->line = line;
 	this->col = col;
+	nodeType = nullptr;
 }
 
  void WhileNode::print(ostream &os) {
@@ -35,3 +37,21 @@ Node* WhileNode::optmize(OptimizationVistor *optimizationVistor)
 void WhileNode::accept(TypeErrorVisitor* typeVisitor) {
 	typeVisitor->visit(this);
 }
+
+  bool WhileNode::type_checking() {
+	  if (this->nodeType != nullptr && dynamic_cast<TypeError*>(this->nodeType) == nullptr) {
+		  //this for second passes, if the current node is free of TypeError no need to re type_check it
+		  this->body->type_checking();
+		  return true; // pass it this time
+	  }
+
+	  if (this->condition->getNodeType()->getTypeId() == BOOLEAN_TYPE_ID) {
+		  this->nodeType = TypesTable::getInstance()->getType(VOID_TYPE_ID);
+		  this->body->type_checking();
+	  }
+	  else {
+		  this->nodeType = new TypeError("Expected Boolean Expression for the Condition. " + string(" line:") + to_string(this->line) + string(",col:") + to_string(this->col));
+		  this->body->type_checking();
+		  return false;
+	  }
+  }
